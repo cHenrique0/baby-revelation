83eb2c3b8ab275980807653bba33ad71631e2cde
Claudio Henrique <claudio_henrique20@hotmail.com>
2024-12-17 20:41:15 -0300

    improving steps

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,8 @@ const confirmarBtn = document.getElementById("confirmarPresenca");
 const actions = document.getElementById("actions");
 const nextBtn = document.getElementById("next");
 const nextBtnBox = document.getElementById("nextButtonBox");
+const submitBtn = document.getElementById("submit");
+const submitBox = document.getElementById("submitBox");
 const etapa1 = document.getElementById("etapa1");
 const etapa2 = document.getElementById("etapa2");
 const etapa3 = document.getElementById("etapa3");
@@ -16,18 +18,17 @@ confirmarBtn.addEventListener("click", () => {
     etapa1.classList.remove("hidden");
     etapa1.classList.add("show");
   }
-  nextBtnBox.classList.toggle("hidden")
-  nextBtnBox.classList.toggle("show")
-  actions.classList.toggle("show");
-  actions.classList.toggle("hidden");
+  nextBtnBox.classList.remove("hidden");
+  nextBtnBox.classList.add("show");
+  submitBox.classList.remove("show");
+  submitBox.classList.add("hidden");
+  actions.classList.remove("show");
+  actions.classList.add("hidden");
 })
 
 nextBtn.addEventListener("click", () => {
 
-  if(etapa >= 1 && etapa < 6) {
-    etapa++;
-    nextBtn.innerText = "Próximo"
-  }
+  etapa++;
 
   if(etapa === 2) {
     etapa1.classList.add("hidden");
@@ -36,6 +37,7 @@ nextBtn.addEventListener("click", () => {
     etapa2.classList.remove("hidden");
     etapa2.classList.add("show");
   }
+
   if(etapa === 3) {
     etapa2.classList.add("hidden");
     etapa2.classList.remove("show");
@@ -43,15 +45,41 @@ nextBtn.addEventListener("click", () => {
     etapa3.classList.remove("hidden");
     etapa3.classList.add("show");
   }
+
   if(etapa === 4) {
     etapa3.classList.add("hidden");
     etapa3.classList.remove("show");
 
-    nextBtn.innerText = "Confirmar"
+    nextBtnBox.classList.add("hidden");
+    nextBtnBox.classList.remove("show");
+    
+    submitBox.classList.remove("hidden");
+    submitBox.classList.add("show");
   
     etapa4.classList.remove("hidden");
     etapa4.classList.add("show");
   }
+
+  if(etapa === 6) {
+    etapa = 0;
+
+    etapa5.classList.add("hidden");
+    etapa5.classList.remove("show");
+
+    nextBtnBox.classList.add("hidden");
+    nextBtnBox.classList.remove("show");
+    
+    submitBox.classList.add("hidden");
+    submitBox.classList.remove("show");
+
+    actions.classList.add("show");
+    actions.classList.remove("hidden");
+  }
+})
+
+submitBtn.addEventListener("click", (event) => {
+
+  etapa++;
 
   if(etapa === 5) {
     etapa4.classList.add("hidden");
@@ -59,17 +87,13 @@ nextBtn.addEventListener("click", () => {
 
     nextBtn.innerText = "Voltar para o início"
 
+    nextBtnBox.classList.remove("hidden");
+    nextBtnBox.classList.add("show");
+    
+    submitBox.classList.add("hidden");
+    submitBox.classList.remove("show");
+
     etapa5.classList.remove("hidden");
     etapa5.classList.add("show")
-  }
-
-  if(etapa === 6) {
-    etapa = 0;
-    etapa5.classList.add("hidden");
-    etapa5.classList.remove("show");
-    nextBtnBox.classList.toggle("show");
-    nextBtnBox.classList.toggle("hidden");
-    actions.classList.toggle("hidden");
-    actions.classList.toggle("show");
   }
 })
